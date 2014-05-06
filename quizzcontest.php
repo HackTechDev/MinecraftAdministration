50a02c79caa03124e5c1fe970177cc533bd51558
Le Sanglier des Ardennes <lesanglierdesardennes@gmail.com>
2014-05-06 15:32:46 +0200

    Update readme and licence

--- quizzcontest.php
+++ quizzcontest.php
@@ -10,6 +10,22 @@ License: GPL2 license
 */
 
 /*
+Copyright 2014  Le Sanglier des Ardennes  (email : lesanglierdesardennes@gmail.com)
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License, version 2, as 
+published by the Free Software Foundation.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+/*
 Initialize/install or uninstall
 */
 
@@ -47,7 +63,8 @@ if ( is_admin() ){
     add_action('admin_menu','quizzcontest_menu');
 
     function quizzcontest_option(){
-         include('admin/quizzcontest_option.php');
+        
+        include('admin/quizzcontest_option.php');
     } 
 }
 
