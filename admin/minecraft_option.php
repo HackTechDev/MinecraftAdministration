ba4e3619b8efbcaffd9a9db467c79acf8d17fb59
Le Sanglier des Ardennes <lesanglierdesardennes@gmail.com>
2014-10-13 20:25:26 +0200

    Update

--- admin/minecraft_option.php
+++ admin/minecraft_option.php
@@ -1,8 +1,8 @@
 <?php 
-    $quizzcontest_data = get_option('quizzcontest_data'); 
+    $minecraft_data = get_option('minecraft_data'); 
 ?>
 <div>
-    <h2>Quizz Contest : Introduction/Réponse</h2>
+    <h2>Minecraft Administration</h2>
     <form method="post" action="options.php">
         <?php wp_nonce_field('update-options'); ?>
 
@@ -10,20 +10,20 @@
             <tr valign="top">
                 <th width="92" scope="row">Version : </th>
                 <td width="406">
-                    <input name="quizzcontest_data['version']" type="text" id="quizzcontest_data['version']" value="<?php echo $quizzcontest_data["'version'"]; ?>" />
+                    <input name="minecraft_data['version']" type="text" id="minecraft_data['version']" value="<?php echo $minecraft_data["'version'"]; ?>" />
                </td>
             </tr>
             <tr valign="top">
                 <th width="92" scope="row">Introduction : </th>
                 <td width="406">
-                    <input name="quizzcontest_data['introduction']" type="text" id="quizzcontest_data['introduction']" value="<?php echo $quizzcontest_data["'introduction'"]; ?>" />
+                    <input name="minecraft_data['introduction']" type="text" id="minecraft_data['introduction']" value="<?php echo $minecraft_data["'introduction'"]; ?>" />
                </td>
             </tr>
 
         </table>
 
         <input type="hidden" name="action" value="update" />
-        <input type="hidden" name="page_options" value="quizzcontest_data" />
+        <input type="hidden" name="page_options" value="minecraft_data" />
 
         <p>
             <input type="submit" value="<?php _e('Save Changes') ?>" />
